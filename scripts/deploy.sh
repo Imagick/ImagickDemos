68785cdf111e885e7dc4aac8b0c6b92769b69d32
Danack <Danack@basereality.com>
2015-01-26 19:53:53 +0000

    Fixed string compare no worky.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,7 @@ if [ "$#" -ge 1 ]; then
     environment=$1
 fi
 
-#need to make dir?
-mkdir -p ./var/cache
-mkdir -p ./var/cache/less
-chown -R imagickdemos ./var
 
-su imagickdemos -c "./scripts/deployAsUser.sh"
+
+su imagickdemos -c "./scripts/deployAsUser.sh ${environment}"
 sh ./autogen/addConfig.sh
--- scripts/deployAsUser.sh
+++ scripts/deployAsUser.sh
@@ -6,10 +6,14 @@ fi
 
 echo "environment is ${environment}";
 
-if [$environment -ne "centos_guest"]; then
+if ["${environment}" -ne "centos_guest"]; then
     #Run Composer install to get all the dependencies.
     /usr/local/bin/php -d allow_url_fopen=1 /usr/sbin/composer install
 fi
+
+#need to make dir?
+mkdir -p ./var/cache/less
+#chown -R imagickdemos ./var
 
 #Generate the config files for nginx, etc.
 /usr/local/bin/php bin/cli.php configurate $environment

5d1df3643aa2979b1879557661ef695fead1dcf6
Danack <Danack@basereality.com>
2015-01-29 13:30:12 +0000

    Removed hardcoding of PHP path.

--- scripts/deployAsUser.sh
+++ scripts/deployAsUser.sh
@@ -8,19 +8,22 @@ echo "environment is ${environment}";
 
 if [ "${environment}" -ne "centos_guest" ]; then
     #Run Composer install to get all the dependencies.
-    /usr/local/bin/php -d allow_url_fopen=1 /usr/sbin/composer install
+    php -d allow_url_fopen=1 /usr/sbin/composer install
 fi
 
 #need to make dir?
 mkdir -p ./var/cache/less
 #chown -R imagickdemos ./var
 
+find . -name "*.sh" -exec chmod 755 {} \;
+
+
 #Generate the config files for nginx, etc.
-/usr/local/bin/php bin/cli.php configurate $environment
+php bin/cli.php configurate $environment
 #Generate some code.
-/usr/local/bin/php ./tool/weaveControls.php
+php ./tool/weaveControls.php
 #Generate the CSS
-/usr/local/bin/php ./tool/compileLess.php
+php ./tool/compileLess.php
 
 
 #todo - make everything other than var be not writable 

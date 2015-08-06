9ee2d6a72e516c0caf45446981ce00cf91ec827d
Danack <Danack@basereality.com>
2015-08-06 00:10:07 +0000

    Added paths.

---
+++ src/Tier/Path/YuiCompressorPath.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Tier\Path;
+
+class YuiCompressorPath
+{
+    private $path;
+
+    public function __construct($yuicompressorPath)
+    {
+        $this->path = $yuicompressorPath;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+}

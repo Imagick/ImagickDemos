2a67175f94277b06fc93cd7de486a5b505ba657d
Danack <Danack@basereality.com>
2019-09-11 17:50:41 +0100

    Don't run debug container in production.

--- runProd.sh
+++ runProd.sh
@@ -2,7 +2,7 @@
 
 
 docker-compose build
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+# docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 
 
-
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --force-recreate imagick_php_backend redis web_server workers

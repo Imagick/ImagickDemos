5bd9d068f796b20d80d4375060cad3b5ead9d027
Danack <Danack@basereality.com>
2021-10-09 14:11:21 +0100

    CSS is not currently built.

--- containers/js_builder/build_prod_assets.sh
+++ containers/js_builder/build_prod_assets.sh
@@ -8,6 +8,6 @@ npm ci
 
 npm run build
 
-npm run sass:build:prod
+# npm run sass:build:prod
 
-gzip -9 -c /var/app/app/public/css/site.css > /var/app/app/public/css/site.css.gz
+# gzip -9 -c /var/app/app/public/css/site.css > /var/app/app/public/css/site.css.gz

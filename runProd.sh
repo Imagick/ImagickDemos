2948bf42fbf83f3b7636df61e6af4bbca95ce341
Danack <Danack@basereality.com>
2021-10-09 13:39:21 +0100

    Make building work.

--- runProd.sh
+++ runProd.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 
+# This needs to be built first, as docker doesn't resolve dependencies
+docker-compose build imagick_php_base
+
+# Build all other containers.
 docker-compose build
+
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate installer
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate js_builder
 

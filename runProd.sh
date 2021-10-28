600c2765d46858ac5db567bd4aafd8dde6dc1c40
Danack <Danack@basereality.com>
2021-10-28 16:56:09 +0100

    Correct prod deployment.

--- runProd.sh
+++ runProd.sh
@@ -3,7 +3,8 @@
 
 # This needs to be built first, as docker doesn't resolve dependencies
 # docker-compose build --no-cache imagick_php_base
-docker-compose build imagick_php_base
+docker-compose build imagick_php_base_im6
+docker-compose build imagick_php_base_im7
 
 # Build all other containers.
 docker-compose build
@@ -11,7 +12,7 @@ docker-compose build
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate installer
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate js_builder
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --force-recreate imagick_php_backend web_server
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --force-recreate --remove-orphans imagick_php_backend_im6 imagick_php_backend_im7 nginx
 
 # redis
 # workers

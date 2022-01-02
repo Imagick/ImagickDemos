499251029d1463d5b8ec4dd53291f415fb34128a
Danack <Danack@basereality.com>
2022-01-02 00:29:17 +0000

    Redis is needed.

--- runProd.sh
+++ runProd.sh
@@ -16,7 +16,9 @@ docker-compose build --no-cache imagick_php_base_im7
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate installer
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate js_builder
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --force-recreate --remove-orphans imagick_php_backend_im6 imagick_php_backend_im7 nginx
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --force-recreate --remove-orphans imagick_php_backend_im6 imagick_php_backend_im7 nginx redis
+
+# docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build --force-recreate --remove-orphans imagick_php_backend_im6 imagick_php_backend_im7 nginx redis
 
 # redis
 # workers

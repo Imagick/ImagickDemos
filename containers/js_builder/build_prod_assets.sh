ee56fd4a454d9c22016cdf0da86a313b64499b37
Danack <Danack@basereality.com>
2022-01-12 19:14:20 +0000

    Fix one prod issue...

--- containers/js_builder/build_prod_assets.sh
+++ containers/js_builder/build_prod_assets.sh
@@ -6,8 +6,8 @@ cd /var/app
 
 npm ci
 
-npm run build
+npm run build:prod
 
-# npm run sass:build:prod
+npm run sass:build:prod
 
-# gzip -9 -c /var/app/app/public/css/site.css > /var/app/app/public/css/site.css.gz
+gzip -9 -c /var/app/public/css/site.css > /var/app/public/css/site.css.gz

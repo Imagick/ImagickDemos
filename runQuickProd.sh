58b4a86db3c8a574f13dcdc6c636d11207747a6a
Danack <Danack@basereality.com>
2022-08-31 13:07:51 +0100

    Quicker build. Don't always need to recompile imagemagick.

---
+++ runQuickProd.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+# This needs to be built first, as docker doesn't resolve dependencies
+# docker-compose build --no-cache imagick_php_base
+
+# docker-compose build --no-cache imagick_php_base_im6
+# docker-compose build --no-cache imagick_php_base_im7
+
+docker-compose build imagick_php_base_im6
+docker-compose build imagick_php_base_im7
+
+# Build all other containers.
+
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate installer
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up  --build --force-recreate js_builder
+
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --force-recreate --remove-orphans imagick_php_backend_im6 imagick_php_backend_im7 nginx redis
+

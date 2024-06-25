0c3a44ec5f6a28c5d7de1afbecde311fe69ff1a4
Danack <Danack@basereality.com>
2024-06-25 14:21:02 +0100

    Prevent numbers entered without leading zeroes from blowing up the image.

--- public/tsx/components/Number.tsx
+++ public/tsx/components/Number.tsx
@@ -11,7 +11,37 @@ export interface NumberProps {
 
 export class Number extends Component<NumberProps, {}> {
 
-    updateValue(value:string|number ) {
+    updateBlurValue(value:string|number) {
+
+        // if a user enters a string which is not a valid number
+        // we force the value to be a floating point number
+        if (typeof value === "string") {
+            value = parseFloat(value);
+        }
+
+        // Force number to be zero instead of NaN.
+        if (isNaN(value) === true) {
+            value = 0;
+        }
+
+        if (this.props.min !== undefined) {
+            if (value < this.props.min) {
+                value = this.props.min;
+            }
+        }
+        if (this.props.max !== undefined) {
+            if (value > this.props.max) {
+                value = this.props.max;
+            }
+        }
+
+        this.props.updateFn(value);
+    }
+
+    // This function only checks the bounds. It does not alter the value
+    // as trying to do that while the user is still typing, leads to
+    // annoying behaviour, as the cursor changes position.
+    updateValue(value:string|number) {
         if (this.props.min !== undefined) {
             if (value < this.props.min) {
                 value = this.props.min;
@@ -33,8 +63,13 @@ export class Number extends Component<NumberProps, {}> {
                 type="number" step="1"
                 name={this.props.name}
                 value={this.props.value}
-                // @ts-ignore: blah blah
-                onchange={(event) => this.updateValue(event.target.value) }
+                // @ts-ignore: It would be nice to validate the value is
+                // in the correct range on every input. but for now, only
+                // validate when focus is lost, to prevent weird interactions
+                // between the users' keypresses and the validate code.
+                oninput={(event) => this.updateValue(event.target.value) }
+              // @ts-ignore: blah blah
+                onBlur={(event) => this.updateBlurValue(event.target.value) }
             />
         </span>;
     }

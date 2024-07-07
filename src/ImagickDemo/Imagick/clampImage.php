8ff945939f40e9abd0b3866cac687453ed2bfece
Danack <Danack@basereality.com>
2024-07-07 22:26:18 +0200

    Added explanation of why ClampImage() might be used.

--- src/ImagickDemo/Imagick/clampImage.php
+++ src/ImagickDemo/Imagick/clampImage.php
@@ -14,7 +14,15 @@ class clampImage extends Example
 
     public function renderDescription()
     {
-        return "ClampImage() restricts the color range from 0 to the quantum depth. aka I have no idea";
+        $content = "<p>ClampImage() restricts the color range from 0 to the quantum depth.</p>";
+
+        $content .= "<p>This is probably of use when adjusting the brightness of an image. Under certain circumstances it is possible for an image to have pixels that are 'brighter' than the maximum (a.k.a. quantum) range. I think when ImageMagick is compiled with HDRI mode enabled.</p>";
+
+        $content .= "<p>Some image processing that ImageMagick can do, just do not work when colours are brighter than maximum. In particular I think that the hue calculations done in modulateImage just don't work as expected, as the maths inside them assumes that 'maximum brightness' is the, well, maximum brightness.</p>";
+
+        $content .= "<p>The example below does <b>not</b> show this usage. Someone needs </p>";
+        
+        return $content;
     }
 
     public function useImageControlAsOriginalImage()
